ecbc30a818b42778ae16accb31fb7cc1c483c8a1
ntIgnant <ignanttt@gmail.com>
2024-06-26 13:00:37 +0200

    IMPROVED | Diplay-Option Function defined and car dictionary created

--- Programming_Task/Car_Rental_Management_System.py
+++ Programming_Task/Car_Rental_Management_System.py
@@ -1,13 +1,32 @@
 class Cars:
 
-    car_dict = ({"Car_ID": 1, "Car_Name": "Toyota Corolla", "Availability": "Available"},
-            {"Car_ID": 2, "Car_Name": "Ford Mustang", "Availability": "Available"},
-            {"Car_ID": 3, "Car_Name": "BMW X5", "Availability": "Available"})
+    car_dict = ({"ID": 1, "Name": "Toyota Corolla", "Availability": "Available"},
+            {"ID": 2, "Name": "Ford Mustang", "Availability": "Available"},
+            {"ID": 3, "Name": "BMW X5", "Availability": "Available"})
 
-    def display_available_cars(self):
-        for value in Cars.car_dict:
-            print(value)
+    def menu_options():
+        options = ["Display Available Cars", "Rent a Car", "Return a Car", "View Your Rented Cars", "Exit"]
+        index = 1
+        for opt in options:
+            print(f"{index}. {opt}")
+            index += 1
+
+        while True:
+            user_choice = int(input("Enter choice: "))
+            try:
+                if user_choice in range(1, len(options)+1):
+                    break
+                else:
+                    print("Please Enter a Valid Option")
+                    
+            except ValueError:
+                print("Please Enter a Number")
+
+    def display_cars():
+        for car in Cars.car_dict:
+            print(car)
 
 
-Cars.display_available_cars()
-# ???
+Cars.menu_options()
+Cars.display_cars()
+
