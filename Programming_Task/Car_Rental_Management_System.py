8be658823313060b8b4034f08c46f5f9c89fa437
ntIgnant_Asp <ignanttt@gmail.com>
2024-06-27 22:18:27 +0200

    IMPROVED | Functions defined and added

--- Programming_Task/Car_Rental_Management_System.py
+++ Programming_Task/Car_Rental_Management_System.py
@@ -1,12 +1,36 @@
 class Cars:
+    # def __init__(self, car_id, car_name, availability="Available"):
+    #     self.car_id = car_id
+    #     self.car_name = car_name
+    #     self.availability = availability
+
     car_dict = (
         {"ID": 1, "Name": "Toyota Corolla", "Availability": "Available"},
         {"ID": 2, "Name": "Ford Mustang", "Availability": "Available"},
         {"ID": 3, "Name": "BMW X5", "Availability": "Available"}
     )
+
     def display_cars(self):
-        for car in self.car_dict:
+        for car in Cars.car_dict:
             print(car)
+
+    def rent_car(self):
+        while True:
+            try:
+                username = input("Enter Your Name: ")
+                car_id = int(input("Enter Car ID to Rent: "))
+                for car in self.car_dict:
+                    if car["ID"] == car_id:
+                        if car["Availability"] == "Available":
+                            car["Availability"] = "Not Available"
+                            print(f"Car {car_id} rented to {username}")
+                            break
+                    else:
+                        print(f"Car ID '{car_id}' not found")
+                        break
+            except ValueError:
+                print("Please enter a number for the car ID")
+
 
     def menu_options(self):
         options = ["Display Available Cars", "Rent a Car", "Return a Car", "View Your Rented Cars", "Exit"]
@@ -26,10 +50,7 @@ class Cars:
                 print("Please Enter a Number")
 
 
-
-
-
 cars_instance = Cars()
-
 cars_instance.menu_options()
-cars_instance.display_cars()
+cars_instance.rent_car()
+
