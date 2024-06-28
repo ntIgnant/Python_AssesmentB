45babd611d697d1cf3c31edf0fb1029c6edebafa
ntIgnant_Asp <ignanttt@gmail.com>
2024-06-28 23:13:42 +0200

    IMPROVED | errors fixed and rent_car function added

--- Programming_Task/Car_Rental_Management_System.py
+++ Programming_Task/Car_Rental_Management_System.py
@@ -1,49 +1,50 @@
 class Cars:
-    # def __init__(self, car_id, car_name, availability="Available"):
-    #     self.car_id = car_id
-    #     self.car_name = car_name
-    #     self.availability = availability
-
-    car_dict = (
-        {"ID": 1, "Name": "Toyota Corolla", "Availability": "Available"},
-        {"ID": 2, "Name": "Ford Mustang", "Availability": "Available"},
-        {"ID": 3, "Name": "BMW X5", "Availability": "Available"}
-    )
+    def __init__(self):
+        self.car_dict = {
+            1: {"Name": "Toyota Corolla", "Availability": "Available"},
+            2: {"Name": "Ford Mustang", "Availability": "Available"},
+            3: {"Name": "BMW X5", "Availability": "Available"}
+        }
 
     def display_cars(self):
-        for car in Cars.car_dict:
-            print(car)
+        for car_id, car_info in self.car_dict.items():
+            print(f"Car ID: {car_id}, Name: {car_info['Name']}, Availability: {car_info['Availability']}")
+            # For each id of the dictionary, and for each information of each id, it will print out the information
+            # with the specific keys in between brackets []
 
     def rent_car(self):
         while True:
             try:
                 username = input("Enter Your Name: ")
                 car_id = int(input("Enter Car ID to Rent: "))
-                for car in self.car_dict:
-                    if car["ID"] == car_id:
-                        if car["Availability"] == "Available":
-                            car["Availability"] = "Not Available"
-                            print(f"Car {car_id} rented to {username}")
-                            break
-                    else:
-                        print(f"Car ID '{car_id}' not found")
+                if car_id in self.car_dict:
+                    target_car = self.car_dict[car_id]
+                    if target_car["Availability"] == "Available":
+                        target_car["Availability"] = "Not Available"
+                        print(f"Car {car_id} rented to {username}")
                         break
+                    else:
+                        print("The current car is not available")
+                else:
+                    print(f"Car ID '{car_id}' not found, please try again")
+
             except ValueError:
                 print("Please enter a number for the car ID")
 
-
     def menu_options(self):
-        options = ["Display Available Cars", "Rent a Car", "Return a Car", "View Your Rented Cars", "Exit"]
-        index = 1
-        for opt in options:
-            print(f"{index}. {opt}")
-            index += 1
-
         while True:
+            options = ["Display Available Cars", "Rent a Car", "Return a Car", "View Your Rented Cars", "Exit"]
+            index = 1
+            for opt in options:
+                print(f"{index}. {opt}")
+                index += 1
             try:
                 user_choice = int(input("Enter choice: "))
                 if user_choice in range(1, len(options) + 1):
-                    break
+                    if user_choice == 1:
+                        Cars.display_cars(self)
+                    elif user_choice == 2:
+                        Cars.rent_car(self)
                 else:
                     print("Please Enter a Valid Option")
             except ValueError:
@@ -52,5 +53,3 @@ class Cars:
 
 cars_instance = Cars()
 cars_instance.menu_options()
-cars_instance.rent_car()
-
--- Programming_Task/secTry.py
+++ Programming_Task/secTry.py
@@ -1,20 +1,16 @@
-class Cars:
-    def __init__(self, car_id, car_name, availability="Available", user_rent=False):
-        self.car_id = car_id
-        self.car_name = car_name
-        self.availability = availability
-        self.user_rent = user_rent
-
-    def __str__(self):
-        print(f"Car ID: {self.car_id}, Name: {self.name}, Availability: {self.availability}")
-
-
-class System:
-    def __init__(self):
-        self.    car_dict = (
-        {"ID": 1, "Name": "Toyota Corolla", "Availability": "Available"},
-        {"ID": 2, "Name": "Ford Mustang", "Availability": "Available"},
-        {"ID": 3, "Name": "BMW X5", "Availability": "Available"}
-        )
-
-# ??
+# def rent_car(self):
+#     while True:
+#         try:
+#             username = input("Enter Your Name: ")
+#             car_id = int(input("Enter Car ID to Rent: "))
+#             for car in self.car_dict:
+#                 if car["ID"] == car_id:
+#                     if car["Availability"] == "Available":
+#                         car["Availability"] = "Not Available"
+#                         print(f"Car {car_id} rented to {username}")
+#                         break
+#                 else:
+#                     print(f"Car ID '{car_id}' not found")
+#                     break
+#         except ValueError:
+#             print("Please enter a number for the car ID")
